a630e5c3f389a3c86696d0001cc072800276de2a
Mosalem2004 <mas2022mas2004@gmail.com>
2023-09-01 23:27:49 +0300

    Salem

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,9 @@
 import { View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
-import Categories from "../componats/Categories";
-import SortCategories from "../componats/SortCategories";
-import Destinations from "../componats/Destinations";
-import Header from "../componats/Header";
+import Categories from "../components/Categories";
+import SortCategories from "../components/SortCategories";
+import Destinations from "../components/Destinations";
+import Header from "../components/Header";
 import tw from "twrnc";
 export default function Home() {
   return (
